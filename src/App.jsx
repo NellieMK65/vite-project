a6cfdffb5a230c100cbf3d35017c78e8bb8a3228
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-04-25 15:40:26 +0300

    added chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from '@chakra-ui/react';
+
 function App() {
-	return <>Vite project</>;
+	return (
+		<>
+			Vite project
+			<Button>Submit button</Button>
+		</>
+	);
 }
 
 export default App;
